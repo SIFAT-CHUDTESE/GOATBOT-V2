014d3ece019e5860956ff6bf182b2e10f2f7ccca
乛 కỉϝꫝт ゎ <sifatx404@gmail.com>
2026-06-21 20:01:34 +0000

    fixed ✅

---
+++ replit.nix
@@ -0,0 +1,16 @@
+{pkgs}: {
+  deps = [
+    pkgs.libuuid
+    pkgs.gnumake
+    pkgs.gcc
+    pkgs.librsvg
+    pkgs.giflib
+    pkgs.libjpeg
+    pkgs.libpng
+    pkgs.pixman
+    pkgs.pango
+    pkgs.cairo
+    pkgs.pkg-config
+    pkgs.python3
+  ];
+}
